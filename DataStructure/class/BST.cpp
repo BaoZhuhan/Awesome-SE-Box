8b52c2096374d62dc81bb030111693bc92bc3f8e
bao zhuhan <baozhuhan2004@163.com>
2024-05-11 09:01:19 +0800

    update BST

--- DataStructure/class/BST.cpp
+++ DataStructure/class/BST.cpp
@@ -8,9 +8,43 @@ typedef struct node{
 
 typedef BinNode* BinTree;
 
-BinTree Find(BinTree BST , int x){
+/*recursion*/
+BinTree Find(BinTree BST, int x){
     if(!BST) return NULL;
-    if(x > BST->data , x){
-
+    if(x > BST->data){
+        return Find(BST->Right, x);
     }
+    else if(x < BST->data){
+        return Find(BST->Left, x);
+    }
+    else{
+        return BST;
+    }
+}
+
+/*iteration*/
+BinTree Find(BinTree BST, int x){
+    while(BST){
+        if(x > BST->data) BST = BST->Right;
+        else if(x < BST->data) BST = BST->Left;
+        else break;
+    }
+    return BST;
+}
+
+/*recursion*/
+struct node* findMin(struct node* BST){
+    if(!BST) return nullptr;
+    else if(!BST->Right) return BST;
+    else return findMin(BST->Left);
+}
+
+/*iteration*/
+struct node* findMin(struct node* BST){
+    if(BST){
+        while(BST->Right){
+            BST = BST->Right;
+        }
+    }
+    return BST;
 }
