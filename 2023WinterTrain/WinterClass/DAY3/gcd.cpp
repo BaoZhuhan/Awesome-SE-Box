0c31710d5a055b16aba6ad8e0956702ecb39d333
Bao Zhuhan <baozhuhan2004@163.com>
2024-08-12 16:08:48 +0800

    Summary

--- 2023WinterTrain/WinterClass/DAY3/gcd.cpp
+++
@@ -1,4 +0,0 @@
-using ll = long long ;
-ll gcd(ll a , ll b){
-    return b == 0 ? a : gcd(b , a%b);
-}
--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("%f" , sqrt(4.0));
-    return 0;
-}
