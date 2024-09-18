e6dd8e7b35c8d1ef11d96c6409d1f1b49ba535b6
Bao Zhuhan <baozhuhan2004@163.com>
2024-09-18 14:24:54 +0800

    更新“叮叮书店素材”

--- 07DataStructureAlgo/Class/String/school_kmp.c
+++ 07DataStructureAlgo/Class/String/school_kmp.c
@@ -5,8 +5,25 @@ typedef struct node{
     int length;
 }SString;
 
+int* my_pre(char* s, int* pi){
+    pi = (int*)malloc(strlen(s) * sizeof(int));
+    pi[0] = 0;
+    int len = strlen(s);
+
+    for(int i = 1; i < len; i++){
+        int j = pi[i - 1];
+        while(j > 0 && s[i] != s[j]){
+            j = pi[j - 1];
+        }
+        if(s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+
 void get_next(SString T, int* next){
-    int i = 0;
+    int i = 1;
     int j = 0;
     next[1] = 0;
     while(i < T.length){
